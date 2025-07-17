5e48204c2b4a2b50af82d63bcfc9440eeb30bd00
circval <noah.arnett1@gmail.com>
2025-07-17 13:57:34 -0700

    D: added fps cap

--- main.py
+++ main.py
@@ -3,18 +3,21 @@ import pygame
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     print("Starting Asteroids!")
     print(f"Screen width:", SCREEN_WIDTH)
     print(f"Screen height:", SCREEN_HEIGHT)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    
     loop = 1
     while loop == 1:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, (0,0,0))
+        screen.fill((0, 0, 0))
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
