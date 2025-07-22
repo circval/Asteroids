5b64f1b8c217e6b2250da0eca3dc531f5d800eb0
circval <noah.arnett1@gmail.com>
2025-07-22 15:02:50 -0700

    I: added groups and updated player drawing

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ def main():
     print(f"Screen height:", SCREEN_HEIGHT)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    
     loop = 1
     player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
 
@@ -19,8 +23,8 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        drawable.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
--- player.py
+++ player.py
@@ -5,22 +5,25 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.image = pygame.Surface((self.radius * 2, self.radius * 2))
+        self.rect = self.image.get_rect()
+           
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
-        a = self.position + forward * self.radius
-        b = self.position - forward * self.radius - right
-        c = self.position - forward * self.radius + right
+        a = pygame.Vector2(self.image.get_width() / 2, self.image.get_height() / 2) + forward * self.radius
+        b = pygame.Vector2(self.image.get_width() / 2, self.image.get_height() / 2) - forward * self.radius - right
+        c = pygame.Vector2(self.image.get_width() / 2, self.image.get_height() / 2) - forward * self.radius + right
         return [a, b, c]
-    
-    def draw(self, screen):
-        pygame.draw.polygon(screen, "white", self.triangle(), 2)
 
     def rotate(self, dt):
         self.rotation += (PLAYER_TURN_SPEED * dt)
     
     def update(self, dt):
+        self.rect.center = self.position
+        self.image.fill((0, 0, 0, 0))
+        pygame.draw.polygon(self.image, "white", self.triangle(), 2)
         keys = pygame.key.get_pressed()
         if keys[pygame.K_a]:
             rev_dt = dt * -1
@@ -30,8 +33,8 @@ class Player(CircleShape):
         if keys[pygame.K_w]:
             self.move(dt)
         if keys[pygame.K_s]:
-            self.move(dt)
+            self.move(dt * -1)
             
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
-        self.position += forward * PLAYER_SPEED * dt    
+        self.position += forward * PLAYER_SPEED * dt
