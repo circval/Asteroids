6bd1139cdcd0cd250aafd17f12f2c8a9617c367e
circval <noah.arnett1@gmail.com.com>
2025-07-26 19:00:14 -0700

    I: added asteroid files

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from constants import *
 from player import *
+from asteroid import *
+from asteroidfield import *
 import pygame
 
 def main():
@@ -10,10 +12,14 @@ def main():
     print(f"Screen width:", SCREEN_WIDTH)
     print(f"Screen height:", SCREEN_HEIGHT)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable)
+    AsteroidField.containers = (updatable,)
+    AsteroidField()
     
     loop = 1
     player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
@@ -23,6 +29,8 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        for asteroid in asteroids:
+            asteroid.draw(screen)
         updatable.update(dt)
         drawable.draw(screen)
         pygame.display.flip()
--- player.py
+++ player.py
@@ -5,7 +5,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
-        self.image = pygame.Surface((self.radius * 2, self.radius * 2))
+        self.image = pygame.Surface((self.radius * 3, self.radius * 3))
         self.rect = self.image.get_rect()
            
 
